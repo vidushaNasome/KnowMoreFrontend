821f78fe173fceacf78f7d35d9cc383ea592dea4
Krishmila <madbkjayasinghe97@gmail.com>
2021-03-16 20:53:27 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Page from "./Example/Page";
+import Page1 from "./Example/Page1";
 
 function App() {
   return (
     <div className="App">
      hello world
      HELLO WORLD 2
+        <br/> hiiii
+        <Page/>
+        <Page1/>
     </div>
   );
 }
