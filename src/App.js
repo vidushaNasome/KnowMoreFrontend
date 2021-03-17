113a6b78ee509e5a000cde3d1162c08d305dcae4
Maleesha Costa <maleesha21costa@gmail.com>
2021-03-17 08:28:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Page2 from "./Page2";
 
 function App() {
   return (
     <div className="App">
      hello world
      HELLO WORLD 2
+        <Page2/>
     </div>
   );
 }
