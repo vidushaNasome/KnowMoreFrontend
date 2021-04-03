585bfbd91a8d9f74586ccb1f9df26c0027ceac9b
OshaniMalavige <malavigeoshani@gmail.com>
2021-04-03 09:11:58 +0530

    update files

--- src/RegisterAndLogin/Register.js
+++ src/RegisterAndLogin/Register.js
@@ -3,9 +3,12 @@ import React, {Component} from 'react';
 class Register extends Component {
     render() {
         return (
-            <div><br/><br/><br/><br/>
-            Register
-                
+            <div>
+                <div className="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" className="btn btn-secondary">Student</button>
+                    <button type="button" className="btn btn-secondary">Teacher</button>
+                    <button type="button" className="btn btn-secondary">School/Institute</button>
+                </div>
             </div>
         );
     }
--- src/Repositary/Repositary.js
+++ src/Repositary/Repositary.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
+import './student.css';
 
 class Repositary extends Component {
     render() {
         return (
             <div><br/><br/><br/><br/>
-                <div align="center">Repositary</div>
-
+                <div id="student">
+                    <h4 className="text-center text-secondary">Student</h4>
+                    <form>
+                        <h6 className="text-left text-danger">Created Clusters</h6>
+                        <div className="form-group text-left">
+                            <label>Cluster Name</label>
+                            <button type="submit" className="btn btn-primary">Add Topics</button>
+                        </div>
+                        <div className="form-group text-left">
+                            <label>Topics</label>
+                            -><label>Topic1</label>
+                            <button type="submit" className="btn btn-primary">Add Sessions</button>
+                        </div>
+                    </form>
+                    </div>
             </div>
         );
     }
